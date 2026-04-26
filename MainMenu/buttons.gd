169ffefa696fcdd3398f1299110aabf47593f70d
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2026-04-26 14:13:36 +0200

    Add parallax background

--- MainMenu/buttons.gd
+++ MainMenu/buttons.gd
@@ -1,12 +1,10 @@
 extends VBoxContainer
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
-
+	print("Play!!")
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
-
+	print("Options!!")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
