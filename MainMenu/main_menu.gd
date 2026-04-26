5f3d47c4d9c9cf332b1307e4720fd4872c10c790
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2026-04-26 17:04:56 +0200

    Add buttons to options, and made animation for options

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,7 +1,22 @@
 extends Control
 
+@onready var options: CanvasLayer = $Options
+@onready var menu: CanvasLayer = $CanvasLayer
+
 func _ready() -> void:
 	get_tree().paused = true
 
 func _process(delta: float) -> void:
 	pass
+
+func _on_options_closed() -> void:
+	menu.show()
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
