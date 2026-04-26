06d30343dda6ffd35d03c023ac5e5b2dbc19a38d
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2026-04-26 13:50:27 +0200

    Add buttons to main menu

---
+++ MainMenu/buttons.gd
@@ -0,0 +1,12 @@
+extends VBoxContainer
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
