7e51a39cc1950a1a805aeb3453c3bdb07df6474e
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2026-06-01 18:31:23 +0200

    Add another items and skills to manager

--- Autoloads/items_manager.gd
+++ Autoloads/items_manager.gd
@@ -4,13 +4,41 @@ enum ItemsType
 {
 	TORCH_SWORD,
 	BASE_SWORD,
-	MITHRILL_ARMOR
+	AMETHYST_RING,
+	BOOK_OF_MIGHT,
+	BOOTS,
+	CHESTPLATE,
+	COMMON_SHIRT,
+	EMERALD_AMULET,
+	GOLD_ARMOR,
+	HOLY_BOOTS,
+	MAGIC_ARMOR,
+	MITHRILL_ARMOR,
+	RUNE_OF_DAMAGE,
+	SHARPNESS_AMULET,
+	SIGIL_OF_MIGHT,
+	WIZARD_HAT,
+	WIZARD_STAFF
 }
 
 const ITEMS: Dictionary = {
 	ItemsType.TORCH_SWORD: preload("res://Items/Swords/torch_sword.tres"),
 	ItemsType.BASE_SWORD: preload("res://Items/Swords/base_sword.tres"),
-	ItemsType.MITHRILL_ARMOR: preload("res://Items/PassiveItems/mithrill_armor.tres")
+	ItemsType.AMETHYST_RING: preload("res://Items/PassiveItems/amethyst_ring.tres"),
+	ItemsType.BOOK_OF_MIGHT: preload("res://Items/PassiveItems/book_of_might.tres"),
+	ItemsType.BOOTS: preload("res://Items/PassiveItems/boots.tres"),
+	ItemsType.CHESTPLATE: preload("res://Items/PassiveItems/chestplate.tres"),
+	ItemsType.COMMON_SHIRT: preload("res://Items/PassiveItems/common_shirt.tres"),
+	ItemsType.EMERALD_AMULET: preload("res://Items/PassiveItems/emerald_amulet.tres"),
+	ItemsType.GOLD_ARMOR: preload("res://Items/PassiveItems/gold_armor.tres"),
+	ItemsType.HOLY_BOOTS: preload("res://Items/PassiveItems/holy_boots.tres"),
+	ItemsType.MAGIC_ARMOR: preload("res://Items/PassiveItems/magic_armor.tres"),
+	ItemsType.MITHRILL_ARMOR: preload("res://Items/PassiveItems/mithrill_armor.tres"),
+	ItemsType.RUNE_OF_DAMAGE: preload("res://Items/PassiveItems/rune_of_damage.tres"),
+	ItemsType.SHARPNESS_AMULET: preload("res://Items/PassiveItems/sharpness_amulet.tres"),
+	ItemsType.SIGIL_OF_MIGHT: preload("res://Items/PassiveItems/sigil_of_might.tres"),
+	ItemsType.WIZARD_HAT: preload("res://Items/PassiveItems/wizard_hat.tres"),
+	ItemsType.WIZARD_STAFF: preload("res://Items/PassiveItems/wizard_staff.tres"),
 }
 
 func get_item(index: int) -> ItemData:
--- Autoloads/skills_manager.gd
+++ Autoloads/skills_manager.gd
@@ -3,9 +3,11 @@ extends Node
 enum SkillType
 {
 	NONE,
-	ICE_HAMMER
+	ICE_HAMMER,
+	ICE_PROJECTILE
 }
 
 const SKILLS: Dictionary[SkillType, SkillResource] = {
-	SkillType.ICE_HAMMER: preload("res://Skills/SkillsResources/ice_hammer.tres")
+	SkillType.ICE_HAMMER: preload("res://Skills/SkillsResources/ice_hammer.tres"),
+	SkillType.ICE_PROJECTILE: preload("res://Skills/SkillsResources/ice_projectile.tres")
 }
