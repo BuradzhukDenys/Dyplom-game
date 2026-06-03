0b72d3c30e89c76631880394ed25cae12109b0b4
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2026-06-03 17:21:17 +0200

    Add more skills

--- Autoloads/skills_manager.gd
+++ Autoloads/skills_manager.gd
@@ -4,10 +4,12 @@ enum SkillType
 {
 	NONE,
 	ICE_HAMMER,
-	ICE_PROJECTILE
+	ICE_PROJECTILE,
+	ICE_BLAST
 }
 
 const SKILLS: Dictionary[SkillType, SkillResource] = {
 	SkillType.ICE_HAMMER: preload("res://Skills/SkillsResources/ice_hammer.tres"),
-	SkillType.ICE_PROJECTILE: preload("res://Skills/SkillsResources/ice_projectile.tres")
+	SkillType.ICE_PROJECTILE: preload("res://Skills/SkillsResources/ice_projectile.tres"),
+	SkillType.ICE_BLAST: preload("res://Skills/SkillsResources/ice_blast.tres")
 }
